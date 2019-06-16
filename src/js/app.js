01873dbfeacbca490a1ece6f409f72571a68532a
LukaszTelesinski <lukasztelesinski@gmail.com>
2019-06-16 22:56:22 +0200

    lazy load images

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,7 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+var lazyLoadInstance = new LazyLoad({
+  elements_selector: ".circle, .box__photo, .left-image, .image-up, .image-down, .logo-down"
+});
